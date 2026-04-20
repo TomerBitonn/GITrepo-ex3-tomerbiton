8dd5eead16365bf6363bf9321f55e582d21bda4c
Tomer Biton <tomerbiton2@gmail.com>
2026-04-20 10:00:12 +0300

    Devops course ex3

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+public static void main(String[] args) {
+for (int i=1;i<10;i++) {
+System.out.println("Hello World ..."+i);
+}
+}
+}
+// This is a simple Java program that prints "Hello World ..." followed by the numbers 1 to 9. The main method is the entry point of the program, and it uses a for loop to iterate from 1 to 9, printing the message in each iteration.
---
+++ tomerbiton.java
@@ -0,0 +1,9 @@
+public class tomerbiton {
+    public static void main(String[] args) {
+        String name = "Tomer Biton";
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + ". " + name);
+        }
+    }
+}
