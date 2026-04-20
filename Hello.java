20211ddc46dc551d400dbebf6a3e07a5ab39b8bb
Tomer Biton <tomerbiton2@gmail.com>
2026-04-20 10:28:39 +0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -5,4 +5,4 @@ System.out.println("Hello World ..."+i);
 }
 }
 }
-// This is a simple Java program that prints "Hello World ..." followed by the numbers 1 to 9. The main method is the entry point of the program, and it uses a for loop to iterate from 1 to 9, printing the message in each iteration.
+// check the ci-cd pipeline
